87d5dfd242e32c362f05bbc08a684827a49621e5
Mihai <mihaifcp44279@gmail.com>
2024-11-05 01:31:41 +0000

    Update run.sh
    oooooooooooo

--- run.sh
+++ run.sh
@@ -2,6 +2,8 @@ sudo apt install cmake -y
 git clone https://gitlab.com/nsnam/ns-3-dev.git
 cp -r src ns-3-dev
 cp SimulatorScript.cc ns-3-dev/scratch
+cp ns3testbed.cc.cc ns-3-dev/scratch
+
 cd ns-3-dev
 ./ns3 configure --build-profile=optimized 
 ./ns3
