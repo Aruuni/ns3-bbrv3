9fe6274f450b5b2b0fd3131fe123430f21639d79
Aruuni <mihaifcp44279@gmail.com>
2024-11-08 21:51:08 +0000

    new script

--- run.sh
+++ run.sh
@@ -2,6 +2,7 @@ sudo apt install cmake -y
 git clone https://gitlab.com/nsnam/ns-3-dev.git
 cp -r src ns-3-dev
 cp SimulatorScript.cc ns-3-dev/scratch
+cp CCTestBed.cc ns-3-dev/scratch
 cp ns3testbed.cc.cc ns-3-dev/scratch
 
 cd ns-3-dev
